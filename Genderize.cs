9d3bdce01d4ab885f3cfd41bc2498a0c5589e7f3
Matt Cooper <matt.cooper@halfbrick.com>
2016-03-29 16:16:58 +1000

    Initial commit

---
+++ Genderize.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Genderize
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public class GenderizeResponse
+    {
+        public string name { get; set; }
+        public Gender gender { get; set; }
+        public double probability { get; set; }
+        public int count { get; set; }
+
+        public override string ToString() { return string.Format("{0} {1} {2} {3}", name, gender, probability, count); }
+
+    }
+
+    public static class Genderizer
+    {
+      
+        private static readonly string API_PREFORMATTED = "https://api.genderize.io/?name={0}";
+
+        public static GenderizeResponse Genderize(string name)
+        {
+            var requestURL = String.Format(API_PREFORMATTED, name);
+
+            WebClient wc = new WebClient();
+            var webResponse = wc.DownloadString(requestURL);
+            var result = JsonConvert.DeserializeObject<GenderizeResponse>(webResponse);
+
+            return result;
+        }
+
+        public static Gender GetGender(string name)
+        {
+            var requestURL = String.Format(API_PREFORMATTED, name);
+
+            WebClient wc = new WebClient();
+            var webResponse = wc.DownloadString(requestURL);
+            var result = JsonConvert.DeserializeObject<GenderizeResponse>(webResponse);
+
+            return result.gender;
+        }
+    }
+}
